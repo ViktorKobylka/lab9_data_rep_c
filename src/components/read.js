804df6878513a2e7399d49daaebf49e052264a15
ViktorKobylka <g00425163@atu.ie>
2024-11-29 09:48:52 +0000

    edited read.js

--- src/components/read.js
+++ src/components/read.js
@@ -5,23 +5,25 @@ import axios from "axios";
 const Read = () => {
 
   const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    
+  const reloadData = () => {
     axios.get('http://localhost:4000/api/movies')
-      .then((response) => {
-        console.log(response.data);
-        setMovies(response.data.movies);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    .then((response) => {
+      console.log(response.data);
+      setMovies(response.data.movies);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  }
+  useEffect(() => {
+    reloadData();
   },[]);
 
   return (
     <div>
       <h3>Hello from read component!</h3>
-      <Movies myMovies={movies} />
+      <Movies myMovies={movies} ReloadData={reloadData}/>
     </div>
   );
 }
